410b28f2417900f437e0b480bc38e6f076e78d16
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-06-06 15:56:51 +0200

    Sec11. i18nSelect Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./uncommon-page.component.css']
 })
 export class UncommonPageComponent {
+    //i18n Select
+    public name:string = 'Ayoub';
+    public gender: 'male' | 'female' = 'male';
+    public invitationMap = {
+      male: 'invitarlo',
+      female : 'invitarla'
+    }
 
+    changeClient():void{
+      this.name = 'Melissa';
+      this.gender = 'female';
+    }
 }
