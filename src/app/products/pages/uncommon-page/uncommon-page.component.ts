a6f8877562bfec809ebf8c989534df07a6928b3a
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-06-07 11:47:09 +0200

    Sec11. AsyncPipe ( observable and promise)

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -40,4 +41,19 @@ export class UncommonPageComponent {
       age: '39',
       address: 'Valencia',
     }
+
+    //Async Pipe
+    public myObservableTimer: Observable<number> = interval(2000).pipe(
+      tap(value => console.log('tap:', value))
+    ); //cada 2sg emite un valor
+
+    public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
+      setTimeout( () => {
+        resolve('Tenemos data en la promesa.');
+        console.log('Tenemos data en la promesa.');
+        this.person.name = 'Otro nombre'
+      }, 3500);
+    })
+
+
 }
