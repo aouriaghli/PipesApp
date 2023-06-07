f3088d24d22144409a76bcf9a4879a821a536860
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-06-07 16:19:23 +0200

    Sec12.Pipes personalizados con argumento y sin

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProductsRoutingModule } from './products-routing.module';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { canFlyPipe } from './pipes/can-fly.pipe';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
-import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { OrderComponent } from './pages/order/order.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { ProductsRoutingModule } from './products-routing.module';
+import { SortByPipe } from './pipes/sort-by.pipe';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
+    OrderComponent,
+
+
+    //Pipes
+    canFlyPipe,
+    SortByPipe,
+    ToggleCasePipe,
   ],
   imports: [
     CommonModule,
